0e3c3b0903ebd3e77f76d3a01837383948102d5c
dishui <dishui_git@126.com>
2020-04-03 16:53:56 +0800

    sego

--- nodejieba/nodejieba.go
+++ nodejieba/nodejieba.go
@@ -15,7 +15,7 @@ func cutModuleLoader(vm *motto.Motto) (otto.Value, error) {
 		str, _ := call.Argument(0).ToString()
 		var words []string
 		//s = "小明硕士毕业于中国科学院计算所，后在日本京都大学深造"
-		segments := segmenter.Segment(str)
+		segments := segmenter.Segment([]byte(str))
 		words = sego.SegmentsToSlice(segments, true)
 
 		v, _ := call.Otto.ToValue(words)
