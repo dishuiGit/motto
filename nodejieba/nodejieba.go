52933723905b02500900e42fa9b9ec691f6318f1
dishuiGit <dishui_git@126.com>
2020-05-10 14:00:01 +0800

    Update nodejieba.go

--- nodejieba/nodejieba.go
+++ nodejieba/nodejieba.go
@@ -4,18 +4,24 @@ import (
 	"github.com/dishuiGit/motto"
 	"github.com/robertkrimen/otto"
 	"github.com/huichen/sego"
+	"os"
+    "os/exec"
+	"path/filepath"
 )
 
 func cutModuleLoader(vm *motto.Motto) (otto.Value, error) {
 	fs, _ := vm.Object(`({})`)
+	file, _ := exec.LookPath(os.Args[0])
+	dir := filepath.Dir(file)
 	// 载入词典
 	var segmenter sego.Segmenter
-	segmenter.LoadDictionary("dictionary.txt")
+	segmenter.LoadDictionary(dir+"/dictionary.txt")
 	fs.Set("cut", func(call otto.FunctionCall) otto.Value {
 		str, _ := call.Argument(0).ToString()
 		var words []string
 		//s = "小明硕士毕业于中国科学院计算所，后在日本京都大学深造"
-		words = segmenter.Segment(str)
+		segments := segmenter.Segment([]byte(str))
+		words = sego.SegmentsToSlice(segments, true)
 
 		v, _ := call.Otto.ToValue(words)
 		return v
